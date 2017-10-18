3875e834cd343a5514daac61cda8347763cd1110
Tomasz Pawelec <tpawelec@outlook.com>
2017-10-18 19:09:55 +0200

    working!

--- pesel.py
+++ pesel.py
@@ -3,12 +3,13 @@ import datetime
 # counters
 total = correct = male = female = 0
 invalid_length = invalid_digit = invalid_date = invalid_checksum = 0
+checksum = 0
 
 # date variables
 day = month = year = 0
 
 PESEL_length = 11
-PESEL_weigths= (1, 3, 7, 9, 1, 3, 7, 9, 1, 3)
+PESEL_weigths = (1, 3, 7, 9, 1, 3, 7, 9, 1, 3)
 
 def make_year(m, y):
 
@@ -36,7 +37,6 @@ def last_day(m, y):
     last_of_month = first_of_next_month - datetime.timedelta(days=1)
     return last_of_month
 
-
 file = open("1e3.dat", 'r')
 
 # main processing loop
@@ -53,8 +53,28 @@ for PESEL in file:
     day = int(PESEL[4:6])
     month = int(PESEL[2:4])%20 #modulo!!
     year = make_year(month, PESEL[0:2])
-    print(type(day))
-    #if datetime(day) > last_day(month, year):
-     #   invalid_date += 1
-
     
+    if month > 12:
+        invalid_date += 1
+        continue
+    elif day > last_day(month, year).day:
+        invalid_date += 1
+        continue
+
+    for i in range(PESEL_length - 1):
+        checksum += PESEL_weigths[i] * int(PESEL[i])
+
+    checksum = (10 - (checksum % 10)) % 10
+
+    if checksum != int(PESEL[10]):
+        invalid_checksum += 1
+        continue
+
+    correct += 1
+    if (int(PESEL[0]) % 2) == 0:
+        female += 1
+    else:
+        male += 1
+
+print(total, correct, female, male)
+print(invalid_length, invalid_digit, invalid_date, invalid_checksum)
